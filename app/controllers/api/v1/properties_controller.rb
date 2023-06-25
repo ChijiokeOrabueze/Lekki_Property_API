4bfff4fe37358d0b037b1caa131dfe32cc837665
Chijioke Orabueze <valentineorabueze@gmail.com>
2023-06-25 21:57:10 +0100

    update: completed update on methods

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -22,13 +22,18 @@ module Api
             def create 
 
                 with_validated_params(CreatePropertyContract.new) do |params|
-                
-                    property = Property.new(params)
 
-                    if property.save
-                        render json: {status: "success", message: "successfully fetched all properties.", data: property}, status: :ok
-                    else
-                        render json: {status: "error", message: "Request unsuccessful.", errors: property.errors},status: :bad_request
+                    begin
+                
+                        property = Property.new(params)
+
+                        if property.save
+                            render json: {status: "success", message: "successfully fetched all properties.", data: property}, status: :ok
+                        else
+                            render json: {status: "error", message: "Request unsuccessful.", errors: property.errors},status: :bad_request
+                        end
+                    rescue
+                        render json: {status: "error", message: "Something went wrong. Please try again later.", errors: "Something went wrong. Please try again later."},status: :bad_request
                     end
                 end
 
@@ -38,13 +43,21 @@ module Api
             def update
 
                 with_validated_params(UpdatePropertyContract.new) do |params|
+                    begin
 
-                    property = Property.find(params[:id])
+                        property = Property.find(params[:id])
 
-                    if property.update(params)
-                        render json: {status: "success", message: "successfully fetched all properties.", data: property}, status: :ok
-                    else 
-                        render json: {status: "error", message: "Request unsuccessful.", errors: property.errors}, status: :bad_request
+                        if property.update(params)
+                            render json: {status: "success", message: "successfully fetched all properties.", data: Property.find(params[:id])}, status: :ok
+                        else 
+                            render json: {status: "error", message: "Request unsuccessful.", errors: property.errors}, status: :bad_request
+                        
+                        end
+
+                    rescue ActiveRecord::RecordNotFound
+                        render json: {status: "error", message: "Requested property not found.", errors: "Requested property not found."},status: :bad_request
+                    rescue
+                        render json: {status: "error", message: "Something went wrong. Please try again later.", errors: "Something went wrong. Please try again later."},status: :bad_request
                     
                     end
                 end
@@ -53,14 +66,6 @@ module Api
 
 
             private
-
-            def properties_body
-                params.permit(:property_address, :property_type, :num_bedrooms, :num_sitting_rooms, :num_kitchen, :num_toilets, :num_bathrooms, :owner, :description, :valid_from)
-            end
-
-            def properties_update
-                params.permit(:num_bedrooms, :num_sitting_rooms, :num_kitchen, :num_toilets, :num_bathrooms, :description, :valid_from)
-            end
 
             def with_validated_params(schema)
                 result = schema.call(params.to_unsafe_hash)
